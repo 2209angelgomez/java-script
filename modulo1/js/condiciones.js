b5fc7dfb4f0d6fdcfea6a1228dc4c3117c2d0af1
Roxana Coronel <ricoronel@misena.edu.co>
2018-10-29 17:27:21 -0500

    wiii

---
+++ modulo1/js/condiciones.js
@@ -0,0 +1,42 @@
+/**realizar un script que pida al usuario dos numeros, cual es el mayor y si son iguales */
+/** *
+var numero1=prompt("ingrese un numero");
+var numero2=prompt("ingrese otro numero");
+
+if (numero1<numero2){
+    console.log ("el numero mayor es", numero2);
+}
+else if (numero2 > numero2){
+    console.log("el numero mayor es", numero1);
+    
+}
+else{
+    console.log("los numeros son iguales");
+}
+
+
+
+/**ingresar por pantalla la edad de una persona, si la èrona es mayor de 18 indicar que es mayor de 
+ * edad de lo contrario es menor de edad, si tiene la edad menor o ighual a 33 escribir eres milenial 
+ * si mayor p igaul a 70 eres un anciano, si es mayor o igualñ a 33 ya no eres un melinial*/
+
+ var edad=prompt("ingrese una edad");
+ if (edad<=17){
+     console.log("es menor de edad");
+ }
+
+ else if (edad==18){
+     console.log("es mayor de edad");
+ }
+
+ else if (edad<=33){
+     console.log("eres un milenial")
+ }
+
+else if (edad>=70){
+    console.log("eres un anciano");
+}
+
+else if(edad>=33){
+    console.log("ya no eres un milenial")
+}
