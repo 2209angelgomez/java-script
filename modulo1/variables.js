3f7c86c2301e335e3b976e85c4169c3ae7affcf8
Roxana Coronel <ricoronel@misena.edu.co>
2018-10-29 15:28:45 -0500

    varioables

---
+++ modulo1/variables.js
@@ -0,0 +1,55 @@
+//**pedir datos por teclado*/
+
+var nombre = prompt("Ingrese su nombre");
+console.log("Este es mi nombre", nombre);
+alert(nombre);
+var edad= prompt("Ingrese su edad");
+console.log("Esta es mi edad", edad);
+alert(edad);
+
+//**variables numèricas */
+var num = 5;
+console.log("num", num)
+
+/** variables de texto*/
+var palabras ="palabras";
+console.log("palabras", palabras);
+
+/**variables booleanas */
+var activo = true;
+console.log("activo", activo);
+
+/**variables tipo aarreglo */
+var colores = ["re", "cryan", activo, num];
+console.log("colores", colores);
+
+/**variables tipo objeto u object */
+var vehiculo ={
+    tipo:"automovil",
+    modelo:2018,
+    usado:true
+};
+console.log("vehiculo", vehiculo);
+console.log("tipo de vehiculo",vehiculo.tipo)
+
+
+
+
+/**diferencias entre var y let */
+var apellido ="lola";
+console.log("apellido", apellido);
+apellido=14;
+console.log("apellido numerico", apellido)
+if(true){
+    apellido="Gòmez";
+    console.log("apellido dentro del if", apellido)
+}
+console.log("apellido fuera del if" ,apellido);
+
+var cantidad =34;
+console.log("cantidad num", cantidad);
+if(true){
+    let cantidad=69;
+    console.log("cantidad dentro del if", cantidad);
+}
+console.log("cantidad fuera del if", cantidad)
