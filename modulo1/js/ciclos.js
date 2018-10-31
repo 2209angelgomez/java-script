51ef7508cbaefe272b4325786ac8d4197d81b40a
Roxana Coronel <ricoronel@misena.edu.co>
2018-10-31 09:15:41 -0500

    yaaaaa

--- modulo1/js/ciclos.js
+++ modulo1/js/ciclos.js
@@ -1,5 +1,5 @@
 var rep=prompt("ingrese el número de repeticiones");
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let index = 0; index < rep; index++) {
+    console.log(index);
     
 }
